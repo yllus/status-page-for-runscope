fd23457bf8c912b40a515f170ef18c1b0a9a43b4
Sully Syed <sully.syed@rci.rogers.com>
2015-08-26 11:17:01 -0400

    Add an AJAX loading animation.

--- page-RUNSCOPESTATUS.php
+++ page-RUNSCOPESTATUS.php
@@ -9,7 +9,15 @@ the_post();
 	</div>
 
 	<div id="div_rsp_content" class="rsp_col rsp_span_10_of_12">
-		
+		<p style="font-size: 32px; margin-top: 60px; text-align: center;">
+			Retrieving data from Runscope, please wait...
+		</p>
+
+		<div style="text-align: center;">
+			<div class="dots-loader">
+				&nbsp;
+			</div>
+		</div>
 	</div>
 
 	<div class="rsp_col rsp_span_1_of_12">
